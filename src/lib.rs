1e44537f076c83ec5a2f9fd6e187cde87e1def3f
Luca Rossi <lucarossi147@gmail.com>
2025-07-17 13:58:54 +0200

    feat: added prelude

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,13 @@ pub mod ranged_measurement;
 pub mod uom;
 pub mod percentage;
 pub mod m1d;
+
+// Prelude module
+pub mod prelude {
+    pub use super::measurement::*;
+    pub use super::prefix::*;
+    pub use super::ranged_measurement::*;
+    pub use super::uom::*;
+    pub use super::percentage::*;
+    pub use super::m1d::*;
+}
