e6587254f800e33c4657e788aa6cffdc0ac21cdd
Luca Rossi <lucarossi147@gmail.com>
2026-07-13 14:28:05 +0200

    fix: added default to uom as well

--- src/uom.rs
+++ src/uom.rs
@@ -5,13 +5,13 @@ use uom_derive::Uom;
 /// Trait for units of measurement (UOM).
 ///
 /// Implement this trait for each unit type to provide a string label for the unit.
-pub trait Uom: Clone + Copy + Debug + Serialize + PartialEq + Send + Sync {
+pub trait Uom: Clone + Copy + Debug + Serialize + PartialEq + Send + Sync + Default {
     /// Returns the string label for the unit (e.g., "V" for Volt).
     fn uom() -> String;
 }
 
 /// Represents the unit Adimensional (F).
-#[derive(PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 pub struct Adimensional;
 impl Uom for Adimensional {
     fn uom() -> String {
@@ -20,47 +20,47 @@ impl Uom for Adimensional {
 }
 
 /// Represents the unit Volt (V).
-#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 #[uom(label = V)]
 pub struct Volt;
 
 /// Represents the unit Ampere (A).
-#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 #[uom(label = A)]
 pub struct Ampere;
 
 /// Represents the unit Watt (W).
-#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 #[uom(label = W)]
 pub struct Watt;
 
 /// Represents the unit Second (s).
-#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 #[uom(label = s)]
 pub struct Second;
 
 /// Represents the unit Hertz (Hz).
-#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 #[uom(label = Hz)]
 pub struct Hertz;
 
 /// Represents the unit Ohm (Ω).
-#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 #[uom(label = Ω)]
 pub struct Ohm;
 
 /// Represents the unit Siemens (S).
-#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 #[uom(label = S)]
 pub struct Siemens;
 
 /// Represents the unit Coulomb (C).
-#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 #[uom(label = C)]
 pub struct Coulomb;
 
 /// Represents the unit Farad (F).
-#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Uom, PartialEq, Debug, Clone, Copy, Serialize, Deserialize, Default)]
 #[uom(label = F)]
 pub struct Farad;
 
